466d0a41f65e71ed9ef8817e921c30ab67086c3d
joseeduardoparteka <160857524+joseeduardoparteka@users.noreply.github.com>
2024-11-18 09:05:32 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -6,8 +6,8 @@ criaCartao(
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do paraná?',
+    'A capital do paraná é curitiba'
 )
 
 criaCartao(
