73d2c93326b993c61aeaeda4faf4ad052fded480
joseeduardoparteka <160857524+joseeduardoparteka@users.noreply.github.com>
2024-11-18 09:15:49 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -12,8 +12,8 @@ criaCartao(
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'quantos dias letivos tem em um ano?',
+    'geralmente 200 dias letivos '
 )
 
 criaCartao(
